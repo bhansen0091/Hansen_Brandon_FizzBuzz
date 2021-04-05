afa936d924ec4f68c6a7470fbfc9cf4b9759c2a3
bhansen0091 <bhansen0091@yahoo.com>
2021-04-05 14:06:17 -0500

    initial commit

---
+++ FizzBuzz.java
@@ -0,0 +1,16 @@
+
+public class FizzBuzz {
+
+    public String fizzBuzz(int number) {
+        if (number == 3) {
+            return "Fizz";
+        } else if (number == 5){
+            return "Buzz";
+        } else if (number == 15) {
+            return "FizzBuzz";
+        } else if (number == 2) {
+            return "2";
+        }
+        return "I don't have a reply for that!";
+    }
+}
